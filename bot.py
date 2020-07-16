05303801a6fe5683d1f7b67970c1bd38ea672fd6
eduardo <eduardo12bg@gmail.com>
2020-07-16 18:20:14 -0300

    config module

--- bot.py
+++ bot.py
@@ -1,21 +1,24 @@
 import discord, asyncio, youtube_dl, os
 from discord.ext import commands
+import config
+
+CONFIGS = config.load_config()
 
 
 def get_prefix(bot, msg):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
-    prefixes = ['k.', 'k!', 's.']
+    prefixes = [CONFIGS['prefix']]
     return commands.when_mentioned_or(*prefixes)(bot, msg)
 
 
 bot = commands.Bot(command_prefix=get_prefix, description='Multipurpose Discord Bot')
-exts = ['music']
+exts = CONFIGS['extensions']
 
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='TWICE LIKEY'))
-    print(bot.user.name)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='cassinao'))
+    print(bot.user.name, ' loaded')
 
 
 @commands.is_owner()
@@ -26,4 +29,4 @@ async def _eval(msg, *, cmd):
 for i in exts:
     bot.load_extension(i)
 
-bot.run('NzI2MTM5NTMwNzQ2Mzk2Njky.XwKS-A.5ioZuPA6L6YuF19f7QwNCVMkuGM')
+bot.run(CONFIGS['token'])
